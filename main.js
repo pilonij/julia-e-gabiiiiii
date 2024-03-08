b6076a19f3ebda34deea2851423d6157fc2ebfb8
julia <108429786+pilonij@users.noreply.github.com>
2024-03-08 12:02:06 -0300

    .

--- main.js
+++ main.js
@@ -5,4 +5,9 @@ while(nome_Usuario == ''){//|| nome_user == null
 nome_user = prompt('Qual é o seu nome?');
 }
 
+if(nome_user == null){
+mano.textContent = 'user';
+}else{
+
+}
 mano.textContent = nome_user;
