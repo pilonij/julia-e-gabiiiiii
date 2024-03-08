2c67caa5a9fe3479b70dd1af79b6e476a217c615
julia <108429786+pilonij@users.noreply.github.com>
2024-03-08 11:55:01 -0300

    .

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 var nome_user = '';
 var mano = document.querySelector('span');
 
-while(nome_Usuario == ''){
+while(nome_Usuario == ''){//|| nome_user == null
 nome_user = prompt('Qual é o seu nome?');
 }
 
-texto.textContent = nome_user;
+mano.textContent = nome_user;
