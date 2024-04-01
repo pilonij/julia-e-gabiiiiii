74ed4164849d12be23c03597b7aac59ebe098487
julia <108429786+pilonij@users.noreply.github.com>
2024-04-01 12:18:54 -0300

    .

--- cont.js
+++ cont.js
@@ -1,3 +1,12 @@
 const botoes = document.querySelectorAll('.objetivos');
-console.log(botoes.lenght);
+console.log(botoes.length);
 
+let i = 0;
+
+while(i<botoes.length) {
+i = i+1;
+console.log(i);
+
+
+}
+    
