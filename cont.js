a25e1ca212ef7f01812cc60664ff2e9d1617cd3c
julia <108429786+pilonij@users.noreply.github.com>
2024-04-11 09:03:33 -0300

    .

--- cont.js
+++ cont.js
@@ -7,8 +7,10 @@ for(let i = 0; i<botoes.length;i++) {
 
 console.log(i);
 botoes[i].onclick = function(){
+for(let j = 0; i<botoes.length;j++) {
+botoes[j].classList.remove('ativo');
+}
 botoes[i].classList.add('ativo');
-//botoes[0].classList.remove('ativo');
 }
 }
 
