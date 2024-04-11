550425a39aa892ee46593eb3009c871ff6218e2a
julia <108429786+pilonij@users.noreply.github.com>
2024-04-11 08:49:26 -0300

    .

--- cont.js
+++ cont.js
@@ -6,7 +6,7 @@ console.log(botoes.length);
 for(let i = 0; i<botoes.length;i++) {
 
 console.log(i);
-botoes[1].classList.add('ativo');
+botoes[i].classList.add('ativo');
 //botoes[0].classList.remove('ativo');
 }
 
