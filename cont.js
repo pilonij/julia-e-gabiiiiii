cb17a94cfa912286c6c06ea40fc89d87538e1616
julia <108429786+pilonij@users.noreply.github.com>
2024-04-11 08:28:45 -0300

    j

--- cont.js
+++ cont.js
@@ -1,15 +1,10 @@
 const botoes = document.querySelectorAll('.objetivos');
 console.log(botoes.length);
 
-let i = 0;
+
 
 for(let i = 0; i<botoes.length;i++) {
 
 console.log(i);
 }
 
-{
-    border: 0;
-    border-bottom: 3px solid white ;
-}
-    
