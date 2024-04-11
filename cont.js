5f3f9784ef86a192f1c63d2948adeb4023f2ea49
julia <108429786+pilonij@users.noreply.github.com>
2024-04-11 09:27:19 -0300

    .

--- cont.js
+++ cont.js
@@ -4,17 +4,17 @@ const textos = document.querySelectorAll('.aba-conteudo');
 
 
 
-for(let i = 0; i<botoes.length;i++) {
+for (let i = 0; i < botoes.length; i++) {
 
-console.log(i);
-botoes[i].onclick = function(){
-for(let j = 0; i<botoes.length;j++) {
-botoes[j].classList.remove('ativo');
-textos[j].classList.remove('ativa');
-}
-botoes[i].classList.add('ativo');
-textos[i].classList.add('ativa');
-}
+    console.log(i);
+    botoes[i].onclick = function () {
+        for (let j = 0; j < botoes.length; j++) {
+            botoes[j].classList.remove('ativo');
+            textos[j].classList.remove('ativa');
+        }
+        botoes[i].classList.add('ativo');
+        textos[i].classList.add('ativa');
+    }
 }
 
 
