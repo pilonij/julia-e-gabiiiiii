8d046d919375085fed12e777f9397b6509388ac5
julia <108429786+pilonij@users.noreply.github.com>
2024-04-18 10:24:59 -0300

    .

--- cont.js
+++ cont.js
@@ -19,8 +19,13 @@ for (let i = 0; i < botoes.length; i++) {
 const contadores = document.querySelectorAll('.contador');
 let tempoAtual = new Date();
 let tempoObjetivo1 = new Date ('2024-04-19T00:00:00');
+let tempoFinal = tempoObjetivo1 - tempoAtual;
+let segundos = tempoFinal/1000;
+let minutos = segundos/60;
+let horas = minutos/60;
+let dias = horas/24;
 
-contadores[0].textContent = "tempoObjetivo1 - tempoAtual";
+contadores[0].textContent = dias +"dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos";
 
 
 
