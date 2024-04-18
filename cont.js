851b6b644ae1b25c206eae85be71c12d795d64fb
julia <108429786+pilonij@users.noreply.github.com>
2024-04-18 09:37:19 -0300

    .

--- cont.js
+++ cont.js
@@ -20,7 +20,7 @@ const contadores = document.querySelectorAll('.contador');
 let tempoAtual = new Date();
 let tempoObjetivo1 = new Date ('2024-04-19T00:00:00');
 
-contadores[0].textContent = "tempoObjetivo1 - tempoAtual?";
+contadores[0].textContent = "tempoObjetivo1 - tempoAtual";
 
 
 
