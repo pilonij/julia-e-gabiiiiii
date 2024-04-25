29142f3c0f6516027c4a604f504b2418cfc90e82
julia <108429786+pilonij@users.noreply.github.com>
2024-04-25 09:08:50 -0300

    .

--- cont.js
+++ cont.js
@@ -25,6 +25,10 @@ let minutos = Math.floor(segundos/60);
 let horas = Math.floor(minutos/60);
 let dias = Math.floor(horas/24);
 
+sgundos %= 60;
+munutos %= 60;
+horas %= 24;
+
 contadores[0].textContent = dias +"dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos";
 
 
